1e78e8514b91de68c9e9981563137909a098f8f7
Duco Winterwerp <duco@wiersma-ict.nl>
2025-03-17 10:07:31 +0100

    Added category detail page

---
+++ pages/categories/[id].vue
@@ -0,0 +1,10 @@
+<template>
+    <div v-if="category">
+        <h1>{{ category.name }}</h1>
+    </div>
+</template>
+<script setup lang="ts">
+import type { Category } from '~/domain/category';
+const route = useRoute();
+const {data: category} = await useCustomFetch<Category>(`/api/categories/${route.params.id}`);
+</script>
